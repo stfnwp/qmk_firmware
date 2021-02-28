a81722ebb9f064714c21ad50c480904f44b0ff4d
Stefan Prosiegel <stefan.prosiegel@googlemail.com>
2021-02-28 13:50:27 +0100

    add custom layers

--- keyboards/lily58/keymaps/splitkb-stefan/keymap.c
+++ keyboards/lily58/keymaps/splitkb-stefan/keymap.c
@@ -19,12 +19,16 @@ extern uint8_t is_master;
 #define _LOWER 1
 #define _RAISE 2
 #define _ADJUST 3
+#define _FOO 4
+#define _BAR 5
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
   RAISE,
   ADJUST,
+  FOO,
+  BAR
 };
 
 
@@ -47,10 +51,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_QWERTY] = LAYOUT( \
   KC_ESC,            KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                                      KC_6,     KC_7,    KC_8,    KC_9,         KC_0,    KC_MINS, \
-  LT(RAISE, KC_TAB), KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                                      KC_Y,     KC_U,    KC_I,    KC_O,         KC_P,    KC_BSLS, \
+  LT(_BAR, KC_TAB),  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                                      KC_Y,     KC_U,    KC_I,    KC_O,         KC_P,    KC_BSLS, \
   LOWER,             KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                                      KC_H,     KC_J,    KC_K,    KC_L,         KC_SCLN, KC_QUOT, \
   KC_LCTRL,          KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC,               KC_RBRC,   KC_N,     KC_M,    KC_COMM, KC_DOT,       KC_SLSH,  KC_EQL, \
-                             KC_LGUI, KC_LALT, KC_DEL,  MT(MOD_LSFT, KC_SPC),  MT(MOD_RSFT, KC_ENT), KC_BSPC,  LT(ADJUST, KC_INS),  MT(MOD_RALT, KC_GRV) \
+                             KC_LGUI, KC_LALT, KC_DEL,  MT(MOD_LSFT, KC_SPC),  MT(MOD_RSFT, KC_ENT), KC_BSPC,  LT(_FOO, KC_INS),  MT(MOD_RALT, KC_GRV) \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -89,10 +93,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_RAISE] = LAYOUT( \
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MUTE, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC__MUTE, \
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR, XXXXXXX, \
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, KC_VOLU, \
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, KC__VOLUP, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC__VOLDOWN, \
                              _______, _______, _______,  _______, _______,  _______, _______, _______ \
 ),
 /* ADJUST
@@ -115,6 +119,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, RGB_TOG, RGB_SPI, RGB_HUI, RGB_SAI, RGB_VAI, \
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_MOD, RGB_SPD, RGB_HUD, RGB_SAD, RGB_VAD, \
                              _______, _______, _______, _______, _______,  _______, _______, _______ \
+  ),
+  [_FOO] = LAYOUT( \
+  XXXXXXX, RGB_M_P, RGB_M_B, RGB_M_R,RGB_M_SW, RGB_M_SN,                  RGB_M_K, RGB_M_X, RGB_M_G, RGB_M_T, XXXXXXX, XXXXXXX, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, RGB_TOG, RGB_SPI, RGB_HUI, RGB_SAI, RGB_VAI, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_MOD, RGB_SPD, RGB_HUD, RGB_SAD, RGB_VAD, \
+                             _______, _______, _______, _______, _______,  _______, _______, _______ \
+  ),
+  [_BAR] = LAYOUT( \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC__MUTE, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR, XXXXXXX, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, KC__VOLUP, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC__VOLDOWN, \
+                             _______, _______, _______,  _______, _______,  _______, _______, _______ \
   )
 };
 
@@ -219,6 +237,22 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           layer_on(_ADJUST);
         } else {
           layer_off(_ADJUST);
+        }
+        return false;
+        break;
+    case FOO:
+        if (record->event.pressed) {
+          layer_on(_FOO);
+        } else {
+          layer_off(_FOO);
+        }
+        return false;
+        break;
+    case BAR:
+        if (record->event.pressed) {
+          layer_on(_BAR);
+        } else {
+          layer_off(_BAR);
         }
         return false;
         break;
