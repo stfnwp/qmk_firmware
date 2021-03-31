511968b8ed781cf3633f714f2201e985b17cebc3
Stefan Prosiegel <stefan.prosiegel@googlemail.com>
2021-03-31 13:57:59 +0200

    (lily): move space away from shift

--- keyboards/lily58/keymaps/splitkb-stefan/keymap.c
+++ keyboards/lily58/keymaps/splitkb-stefan/keymap.c
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   LT(_BAR, KC_TAB),  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                                      KC_Y,     KC_U,    KC_I,    KC_O,         KC_P,    KC_BSLS, \
   LOWER,             KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                                      KC_H,     KC_J,    KC_K,    KC_L,         KC_SCLN, KC_QUOT, \
   KC_LCTRL,          KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC,               KC_RBRC,   KC_N,     KC_M,    KC_COMM, KC_DOT,       KC_SLSH,  KC_EQL, \
-                             KC_LGUI, KC_LALT, KC_DEL,  MT(MOD_LSFT, KC_SPC),  MT(MOD_RSFT, KC_ENT), KC_BSPC,  LT(_FOO, KC_INS),  MT(MOD_RALT, KC_GRV) \
+                             KC_LGUI, KC_LALT, KC_SPC,  KC_LSFT,  MT(MOD_RSFT, KC_ENT), KC_BSPC,  LT(_FOO, KC_INS),  MT(MOD_RALT, KC_GRV) \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
