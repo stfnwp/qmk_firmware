87759f3dcb62c0607ca789c9070ede6d00dd547e
Stefan Prosiegel <stefan.prosiegel@googlemail.com>
2021-04-02 10:15:45 +0200

    (lily58): add numblock layer, improve keymap

--- keyboards/lily58/keymaps/splitkb-stefan/config.h
+++ keyboards/lily58/keymaps/splitkb-stefan/config.h
@@ -31,6 +31,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define USE_SERIAL_PD2
 
 #define TAPPING_FORCE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_TERM 200
 
 #undef RGBLED_NUM
--- keyboards/lily58/keymaps/splitkb-stefan/keymap.c
+++ keyboards/lily58/keymaps/splitkb-stefan/keymap.c
@@ -21,6 +21,7 @@ extern uint8_t is_master;
 #define _ADJUST 3
 #define _FOO 4
 #define _BAR 5
+#define _NUM 6
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -28,7 +29,8 @@ enum custom_keycodes {
   RAISE,
   ADJUST,
   FOO,
-  BAR
+  BAR,
+  NUM
 };
 
 
@@ -50,11 +52,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QWERTY] = LAYOUT( \
-  KC_ESC,            KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                                      KC_6,     KC_7,    KC_8,    KC_9,         KC_0,    KC_MINS, \
-  LT(_BAR, KC_TAB),  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                                      KC_Y,     KC_U,    KC_I,    KC_O,         KC_P,    KC_BSLS, \
-  LOWER,             KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                                      KC_H,     KC_J,    KC_K,    KC_L,         KC_SCLN, KC_QUOT, \
-  KC_LCTRL,          KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC,               KC_RBRC,   KC_N,     KC_M,    KC_COMM, KC_DOT,       KC_SLSH,  KC_EQL, \
-                             KC_LGUI, KC_LALT, KC_SPC,  KC_LSFT,  MT(MOD_RSFT, KC_ENT), KC_BSPC,  LT(_FOO, KC_INS),  MT(MOD_RALT, KC_GRV) \
+  KC_ESC,            KC_1,   KC_2,    KC_3,    KC_4,         KC_5,                                      KC_6,     KC_7,         KC_8,    KC_9,         KC_0,    KC_MINS, \
+  LT(_BAR, KC_TAB),  KC_Q,   KC_W,    KC_E,    KC_R,         KC_T,                                      KC_Y,     KC_U,         KC_I,    KC_O,         KC_P,    KC_BSLS, \
+  LOWER,             KC_A,   KC_S,    KC_D,    LCTL_T(KC_F), KC_G,                                      KC_H,     LCTL_T(KC_J), KC_K,    KC_L,         KC_SCLN, KC_QUOT, \
+  KC_LSFT,           KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC,               KC_RBRC,   KC_N,     KC_M,              KC_COMM, KC_DOT,       KC_SLSH,  KC_EQL, \
+                             KC_LGUI, KC_LALT, NUM, LSFT_T(KC_SPC),  KC_SFTENT,                               KC_BSPC,  LT(_FOO, KC_INS),  MT(MOD_RALT, KC_GRV) \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -133,7 +135,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, KC__VOLUP, \
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC__VOLDOWN, \
                              _______, _______, _______,  _______, _______,  _______, _______, _______ \
-  )
+  ),
+  [_NUM] = LAYOUT( \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, KC_7, KC_8, KC_9, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, KC_4, KC_5, KC_6, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_1, KC_2, KC_3, \
+                             _______, _______, _______,  _______, _______,  _______, _______, KC_0 \
+  ),
 };
 
 int RGB_current_mode;
@@ -256,6 +265,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         }
         return false;
         break;
+    case NUM:
+        if (record->event.pressed) {
+          layer_on(_NUM);
+        } else {
+          layer_off(_NUM);
+        }
+        return false;
+        break;
   }
   return true;
 }
@@ -319,3 +336,4 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     }
   return state;
 }
+
