964e5085e3a7fdf86b66857c97b44f87d0fad6b4
Stefan Prosiegel <stefan.prosiegel@googlemail.com>
2021-04-21 10:18:42 +0200

    (lily58): move lshift and kc_grv

--- keyboards/lily58/keymaps/splitkb-stefan/keymap.c
+++ keyboards/lily58/keymaps/splitkb-stefan/keymap.c
@@ -55,8 +55,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,            KC_1,   KC_2,    KC_3,         KC_4,         KC_5,                                      KC_6,     KC_7,         KC_8,         KC_9,         KC_0,    KC_MINS, \
   LT(_BAR, KC_TAB),  KC_Q,   KC_W,    KC_E,         KC_R,         KC_T,                                      KC_Y,     KC_U,         KC_I,         KC_O,         KC_P,    KC_BSLS, \
   LOWER,             KC_A,   KC_S,    LALT_T(KC_D), LCTL_T(KC_F), KC_G,                                      KC_H,     LCTL_T(KC_J), LALT_T(KC_K), KC_L,         KC_SCLN, KC_QUOT, \
-  KC_GRV,            KC_Z,   KC_X,    KC_C,         KC_V,         KC_B,    KC_LBRC,               KC_RBRC,   KC_N,     KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,  KC_EQL, \
-                             KC_LGUI, KC_INS,       NUM, LSFT_T(KC_SPC),  KC_SFTENT,                               KC_BSPC,  FOO,  KC_RALT \
+  KC_LSHIFT,         KC_Z,   KC_X,    KC_C,         KC_V,         KC_B,    KC_LBRC,               KC_RBRC,   KC_N,     KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,  KC_EQL, \
+                             KC_LGUI, KC_INS,       LT(_NUM, KC_GRV), KC_SPC,  KC_SFTENT,                               KC_BSPC,  FOO,  KC_RALT \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
